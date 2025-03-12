0419476b0003d9152825531144fe932add135f76
Mark Heinonen <heinonenmh@gmail.com>
2025-03-12 12:03:00 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,8 @@ const urlsToCache = [
   "./manifest.json",
   "./icons/icon-192x192.png",
   "./icons/icon-512x512.png",
-  "./service-worker.js"
+  "./service-worker.js",
+  "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
 ];
 
 self.addEventListener("install", (event) => {
